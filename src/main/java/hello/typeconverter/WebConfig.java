ccb89ff83976333c43abb45e3e53b1663c3c6335
K-MHoon <azmria117@gmail.com>
2021-10-07 21:25:04 +0900

    feat : Using formatter to HTML
    - Spring base Formatter
    - @NumberFormat
    - @DateTimeFormat

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        // 주석처리 우선순위
+        // 컨버터가 우선순위를 먼저 먹기 때문에, 포메터가 안먹는다.
+        //        registry.addConverter(new StringToIntegerConverter());
+        //        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new StringToIntegerConverter());
         registry.addConverter(new IpPortToStringConverter());
-        registry.addConverter(new IntegerToStringConverter());
+
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
